ca2cde44f09c6bcd041fde2355951cb11cd2ff34
JuliaAlmeida-dev <julia.almeidabm@gmail.com>
2025-06-01 16:00:17 -0300

    js
    e another things

---
+++ js.js
@@ -0,0 +1,29 @@
+function soma() {
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo').innerHTML = a + b;
+}
+
+function subtracao() {
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo').innerHTML = a - b;
+}
+
+function multiplicacao() {
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo').innerHTML = a * b;
+}
+
+function division(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo').innerHTML = a/b;
+}
+
+function potenciacao(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo').innerHTML = a**b;
+}
